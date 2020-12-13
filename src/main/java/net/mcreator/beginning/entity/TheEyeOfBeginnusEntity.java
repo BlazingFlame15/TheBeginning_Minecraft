e987ce6415ee85a704cc61528b93a7695e5b4258
CertainReality <hyunsuheun@gmail.com>
2020-12-12 20:51:16 -0800

    beginnuys eye changes

--- src/main/java/net/mcreator/beginning/entity/TheEyeOfBeginnusEntity.java
+++ src/main/java/net/mcreator/beginning/entity/TheEyeOfBeginnusEntity.java
@@ -42,7 +42,8 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.CreatureAttribute;
-import net.minecraft.client.renderer.entity.model.GhastModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.block.BlockState;
 
@@ -56,6 +57,9 @@ import java.util.Random;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.EnumSet;
+
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.mojang.blaze3d.matrix.MatrixStack;
 
 @BeginningModElements.ModElement.Tag
 public class TheEyeOfBeginnusEntity extends BeginningModElements.ModElement {
@@ -78,11 +82,13 @@ public class TheEyeOfBeginnusEntity extends BeginningModElements.ModElement {
 	@SubscribeEvent
 	@OnlyIn(Dist.CLIENT)
 	public void registerModels(ModelRegistryEvent event) {
-		RenderingRegistry.registerEntityRenderingHandler(entity, renderManager -> new MobRenderer(renderManager, new GhastModel(), 0.5f) {
-			@Override
-			public ResourceLocation getEntityTexture(Entity entity) {
-				return new ResourceLocation("beginning:textures/ghasttexture.png");
-			}
+		RenderingRegistry.registerEntityRenderingHandler(entity, renderManager -> {
+			return new MobRenderer(renderManager, new Modelghasr4(), 0.5f) {
+				@Override
+				public ResourceLocation getEntityTexture(Entity entity) {
+					return new ResourceLocation("beginning:textures/ghasttexture.png");
+				}
+			};
 		});
 	}
 	public static class CustomEntity extends MonsterEntity implements IRangedAttackMob {
@@ -275,6 +281,37 @@ public class TheEyeOfBeginnusEntity extends BeginningModElements.ModElement {
 		public void livingTick() {
 			super.livingTick();
 			this.setNoGravity(true);
+		}
+	}
+
+	// Made with Blockbench 3.7.4
+	// Exported for Minecraft version 1.15
+	// Paste this class into your mod and generate all required imports
+	public static class Modelghasr4 extends EntityModel<Entity> {
+		private final ModelRenderer bb_main;
+		public Modelghasr4() {
+			textureWidth = 16;
+			textureHeight = 16;
+			bb_main = new ModelRenderer(this);
+			bb_main.setRotationPoint(0.0F, 24.0F, 0.0F);
+			bb_main.setTextureOffset(0, 0).addBox(-46.0F, -54.0F, -4.0F, 50.0F, 50.0F, 50.0F, 0.0F, false);
+		}
+
+		@Override
+		public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+			// previously the render function, render code was moved to a method below
+		}
+
+		@Override
+		public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue,
+				float alpha) {
+			bb_main.render(matrixStack, buffer, packedLight, packedOverlay);
+		}
+
+		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+			modelRenderer.rotateAngleX = x;
+			modelRenderer.rotateAngleY = y;
+			modelRenderer.rotateAngleZ = z;
 		}
 	}
 }
